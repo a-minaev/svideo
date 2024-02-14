a94b98698dc840175b91e4c7b7e03584942c32fe
a-minaev <143130276+a-minaev@users.noreply.github.com>
2024-02-14 09:01:30 -0800

    add error handling middleware

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/', database);
 
+app.use((err,req, res, next) => {
+    if(!err.status) {
+        err.status = 500;
+    }
+    res.status(err.status).send(err.message);
+})
 app.listen(PORT, () => {
     console.log(`You are now tuned in to svideo on PORT ${PORT}`);
 });
