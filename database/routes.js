37396be17a7257e4c0c45f094aefe1d8b1fe847d
a-minaev <143130276+a-minaev@users.noreply.github.com>
2024-02-14 08:28:45 -0800

    add route for handling query params

--- database/routes.js
+++ database/routes.js
@@ -11,12 +11,16 @@ database.param('id', (req, res, next, id) => {
 })
 
 
-database.get('/movies', (req, res, next) => {
+database.get('/movies', (req, res, next) => { //will use returnMovies
     res.status(200).send('Movie List is in development, check back soon!');
 })
 
 database.get('/movie/:id', (req, res, next) => {
     res.status(200).send(`Sorry, we're having trouble locating the current title`);
+})
+
+database.get('/find', (req, res, next) => { //will be used to handle query params
+    res.status(200).send(`We'll find some movies for ya`);
 })
 
 database.post('/movie', (req, res, next) => {
