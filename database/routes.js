06ce2bc81fa3cc26ab9052642faffa6ad234a476
a-minaev <143130276+a-minaev@users.noreply.github.com>
2024-02-14 06:44:33 -0800

    add param handler for id

--- database/routes.js
+++ database/routes.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const database = express.Router();
 
+database.param('id', (req, res, next, id) => {
+    const movieID = id;
+    // check for id in list
+
+    req.movieID = movieID;
+    next();
+
+})
+
+
 database.get('/movies', (req, res, next) => {
     res.status(200).send('Movie List is in development, check back soon!');
 })
