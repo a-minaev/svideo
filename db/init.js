6345940c0ea42b752915037a01b196276a877f3c
a-minaev <143130276+a-minaev@users.noreply.github.com>
2024-06-06 15:33:53 -0700

    add sequelize and initiate connection

---
+++ db/init.js
@@ -0,0 +1,36 @@
+import { Sequelize, DataTypes } from 'sequelize';
+
+const sequelize = new Sequelize('4Star', 'postgres', 'admin', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection successfull');
+} catch(error) {
+    console.error('Unable to connect :(');
+};
+
+const Movie = sequelize.define('Movie', {
+    //model attributes (column names)
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        allowNull: false
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    year: DataTypes.INTEGER,
+    director: DataTypes.STRING,
+    lead_actor: DataTypes.STRING,
+    support_actor: DataTypes.STRING,
+    genre: DataTypes.STRING,
+    type: DataTypes.STRING
+
+})
+
+sequelize.close();
