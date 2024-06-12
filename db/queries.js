c3080a61ea5c6f440d1181ac3a106ac654045c62
a-minaev <143130276+a-minaev@users.noreply.github.com>
2024-06-12 14:10:58 -0500

    refine findMovie query with switchcase

--- db/queries.js
+++ db/queries.js
@@ -1,10 +1,78 @@
 const Movie = require('./init');
 
 const findMovie = async(req, res, next) => {
-  const movie = await Movie.findAll({
-    where: {
-      title: req.title
-    }
-  });
+  const req.title = req.query.title;
+  const req.director = req.query.director;
+  const req.year = req.query.year;
+
+  switch(true) {
+      
+      case(req.title && req.director && req.year):
+        const movie = await Movie.findAll({
+          where: {
+            title: req.title,
+            director: req.director,
+            year: req.year
+          }
+        });
+
+      case(req.title && req.director):
+        const movie = await Movie.findAll({
+          where: {
+            title : req.title,
+            director : req.director
+          }
+        });
+
+      case(req.title && req.year):
+        const movie = await Movie.findAll({
+          where: {
+            title: req.title,
+            year: req.year
+          }
+        });
+
+      case(req.director && req.year):
+        const movie = await Movie.findAll({
+          where: {
+            director: req.director,
+            year: req.year
+          }
+        });
+
+      case(req.title):
+        const movie = await Movie.findAll({
+          where: {
+            title: req.title
+          }
+        });
+
+      case(req.director):
+        const movie = await Movie.findAll({
+          where: {
+            director: req.director
+          }
+        });
+
+      case(req.year):
+        const movie = await Movie.findAll({
+          where: {
+            year: req.year
+          }
+        });
+
+    default:
+        const movie = await Movie.findAll({
+          
+        });
+  }
+
   next();
+};
+
+// reduntant; can create three variables and execute as neededin findMovie
+const checkQueryParams = (req, res, next) => {
+  switch(true) {
+      case(req.query.title 
+  }
 };
