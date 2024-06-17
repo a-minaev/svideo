0270a747c7b3d1dd97ecc1a49cf3255ec7e6eec5
a-minaev <143130276+a-minaev@users.noreply.github.com>
2024-06-17 14:53:47 -0700

    correct import declaration

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import bodyParser from'body-parser';
-import cors from 'cors';
+import { express } from 'express';
+import { bodyParser }from'body-parser';
+import { cors } from 'cors';
 const app = express();
 const db = require('./db/routes');
 
